7a5c73bc6e61c0565b55df4d6a56a82bafad7171
leejk0924 <akdirdl@gmail.com>
2022-04-07 10:24:52 +0900

    Feat: study chapter03

---
+++ JAVA/src/chapter03/ch2/VIPCustomer.java
@@ -0,0 +1,9 @@
+package chapter03.ch2;
+
+public class VIPCustomer {
+    private int customerID;
+    private String customerName;
+    private String customerGrade;
+    int bonusPoint;
+    double bonusRatio;
+}
