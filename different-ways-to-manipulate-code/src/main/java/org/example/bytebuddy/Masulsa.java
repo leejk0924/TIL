024b5ad22ac2d0c223d0463600634fea9c259a43
leejk0924 <akdirdl@gmail.com>
2022-04-11 14:11:53 +0900

    Feat: bytebyddy implement

--- different-ways-to-manipulate-code/src/main/java/org/example/bytebuddy/Masulsa.java
+++ different-ways-to-manipulate-code/src/main/java/org/example/bytebuddy/Masulsa.java
@@ -1,7 +1,9 @@
 package org.example.bytebuddy;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,13 +12,17 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) {
-//        try {
-//            new ByteBuddy().redefine(Moja.class)
-//                    .method(named("pullout")).intercept(FixedValue.value("Rabbit!"))
-//                    .make().saveIn(new File("C:\\Users\\jk\\Documents\\dev\\TIL\\different-ways-to-manipulate-code\\target\\classes"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        ClassLoader classLoader = Masulsa.class.getClassLoader();
+        TypePool typePool = TypePool.Default.of(classLoader);
+
+        try {
+            new ByteBuddy().redefine(typePool.describe("org.example.bytebuddy.Moja").resolve(),
+                    ClassFileLocator.ForClassLoader.of(classLoader))
+                    .method(named("pullout")).intercept(FixedValue.value("Rabbit!"))
+                    .make().saveIn(new File("C:\\Users\\jk\\Documents\\dev\\TIL\\different-ways-to-manipulate-code\\target\\classes"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(new Moja().pullout());
     }
 
