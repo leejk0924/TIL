aff34d31f99cbd09334215aa6be015330d6e4e08
jk <akdirdl@gmail.com>
2025-06-15 21:05:04 +0900

    redis: Sorted Sets 활용

--- redis/src/main/java/com/example/redis/controller/ApiController.java
+++ redis/src/main/java/com/example/redis/controller/ApiController.java
@@ -2,19 +2,41 @@ package com.example.redis.controller;
 
 
 import com.example.redis.dto.UserProfile;
+import com.example.redis.service.RankingService;
 import com.example.redis.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ApiController {
     private final UserService userService;
+    private final RankingService rankingService;
 
     @GetMapping("/users/{userId}/profile")
     public UserProfile getUserProfile(@PathVariable(value = "userId") String userId) {
         return userService.getUserProfile(userId);
+    }
+
+    @GetMapping("/setScore")
+    public Boolean setScore(
+            @RequestParam String userId,
+            @RequestParam int score
+    ) {
+        return rankingService.setUserScore(userId, score);
+    }
+
+    @GetMapping("/getRank")
+    public Long getUserRank(
+            @RequestParam String userId
+    ) {
+        return rankingService.getUserRanking(userId);
+    }
+
+    @GetMapping("/getTopRanks")
+    public List<String> getTopRanks() {
+        return rankingService.getTopRank(3);
     }
 }
---
+++ redis/src/main/java/com/example/redis/service/RankingService.java
@@ -0,0 +1,33 @@
+package com.example.redis.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class RankingService {
+    private static final String LEADERBOARD_KEY = "leaderBoard";
+    private final StringRedisTemplate redisTemplate;
+
+    public boolean setUserScore(String userId, int score) {
+        ZSetOperations<String, String> zSetOps = redisTemplate.opsForZSet();
+        zSetOps.add(LEADERBOARD_KEY, userId, score);
+        return true;
+    }
+
+    public Long getUserRanking(String userId) {
+        ZSetOperations<String, String> zSetOps = redisTemplate.opsForZSet();
+        return zSetOps.reverseRank(LEADERBOARD_KEY, userId);
+    }
+
+    public List<String> getTopRank(int limit) {
+        ZSetOperations<String, String> zSetOps = redisTemplate.opsForZSet();
+        Set<String> rangeSet = zSetOps.reverseRange(LEADERBOARD_KEY, 0, limit - 1);
+        return List.copyOf(rangeSet);
+    }
+}
